1f07b74bba0ae4ef500aebd34e087faefc771fab
RubenBartelet <73698190+RubenBartelet@users.noreply.github.com>
2022-02-02 22:41:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ gen = 0
 CURRENT_MAP_INFO = []
 
 def random_map():
-    rand = randint(1,3)
+    rand = 1 #change this to randint(1,3) for multiple tracks
     if rand == 1:
         MAP_INFO = [MAP,VIS_MAP,LINES]
         return MAP_INFO 
